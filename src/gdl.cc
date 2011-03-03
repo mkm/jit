5fea9a578e9bc19c05c4019dc5cca01a37e8ebaa
mkm <kragh@diku.dk>
2011-03-04 00:38:54 +0100

    Implement parser that does not yet fully work.

--- src/gdl.cc
+++ src/gdl.cc
@@ -2,36 +2,173 @@
 #include "panic.hh"
 
 namespace GDL {
-  Parser::Parser(std::string& s) :
-    _pos(s.begin()),
-    _end(s.end())
+  std::string tokenTypeString(TokenType type) {
+    switch (type) {
+    case IdentToken:
+      return "IdentToken";
+    case OpToken:
+      return "OpToken";
+    case IntLitToken:
+      return "IntLitToken";
+    case EOFToken:
+      return "EOFToken";
+    default:
+      panic("Unknown token type.\n");
+    }
+  }
+  
+  Token::Token(TokenType type, std::string value) :
+    _type(type),
+    _value(value),
+    _intValue(0)
+  {
+
+  }
+
+  Token::Token(TokenType type, int intValue) :
+    _type(type),
+    _value(),
+    _intValue(intValue)
   {
     
   }
 
+  TokenType Token::type() const {
+    return _type;
+  }
+
+  std::string Token::value() const {
+    return _value;
+  }
+
+  int Token::intValue() const {
+    return _intValue;
+  }
+
+  std::string Token::toString() const {
+    std::string s;
+    s +=  "(";
+    s += tokenTypeString(_type);
+    s += ")";
+    return s;
+  }
+  
+  Tokeniser::Tokeniser(std::string& s) :
+    _pos(s.begin()),
+    _end(s.end()),
+    _tokens()
+  {
+    
+  }
+
+  std::vector<Token> Tokeniser::tokenise() {
+    while (_pos != _end) {
+      char c = *_pos;
+      switch (c) {
+      case '+':
+        add(OpToken, "+");
+        ++_pos;
+        break;
+      default:
+        if (isDigit(c)) {
+          number();
+        } else if (isLetter(c)) {
+          ident();
+        } else if (isWhiteSpace(c)) {
+          ++_pos;
+        } else {
+          panic("Unknown symbol.\n");
+        }
+      }
+    }
+
+    add(EOFToken, 0);
+    
+    return _tokens;
+  }
+
+  void Tokeniser::add(TokenType type, std::string value) {
+    _tokens.push_back(Token(type, value));
+  }
+
+  void Tokeniser::add(TokenType type, int value) {
+    _tokens.push_back(Token(type, value));
+  }
+
+  void Tokeniser::number() {
+    char c = *_pos;
+    int n = 0;
+    do {
+      n *= 10;
+      n += c - '0';
+      ++_pos;
+      c = *_pos;
+    } while (isDigit(c));
+
+    add(IntLitToken, n);
+  }
+
+  void Tokeniser::ident() {
+    char c = *_pos;
+    std::string s;
+    do {
+      s += c;
+      ++_pos;
+      c = *_pos;
+    } while (isLetter(c));
+
+    add(IdentToken, s);
+  }
+
+  bool Tokeniser::isDigit(char c) {
+    return '0' <= c && c <= '9';
+  }
+
+  bool Tokeniser::isLetter(char c) {
+    return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_';
+  }
+
+  bool Tokeniser::isWhiteSpace(char c) {
+    return c == ' ' || c == '\t' || c == '\n';
+  }
+  
+  Parser::Parser(std::string& s) :
+    _pos(Tokeniser(s).tokenise().begin())
+  {
+    
+  }
+
+  void Parser::parseEOF() {
+    if ((*_pos).type() != EOFToken) {
+      error(EOFToken);
+    }
+  }
+  
   IL::Expression* Parser::parseExpression() {
     return parseAddExpression();
   }
 
   IL::Expression* Parser::parseAddExpression() {
-    IL::IntConstExpression* expr = parseIntConstExpression();
-    if (_pos == _end || *_pos == '\n') {
-      return expr;
-    } else if (*_pos == '+') {
-      _pos++;
-      return new IL::AddExpression(expr, parseAddExpression());
+    IL::Expression* left = parseIntConstExpression();
+    if ((*_pos).type() == OpToken && (*_pos).value() == "+") {
+      ++_pos;
+      IL::Expression* right = parseExpression();
+      return new IL::AddExpression(left, right);
     } else {
-      panic("Parse error in add expression.\n");
+      return left;
     }
   }
 
   IL::IntConstExpression* Parser::parseIntConstExpression() {
-    char c = *_pos;
-    if ('0' <= c && c <= '9') {
-      _pos++;
-      return new IL::IntConstExpression(c - '0');
+    if ((*_pos).type() == IntLitToken) {
+      return new IL::IntConstExpression((*_pos).intValue());
     } else {
-      panic("Parse error in int const expression.\n");
+      error(IntLitToken);
     }
+  }
+
+  void Parser::error(TokenType expected) {
+    std::string s = "Parser: expected '" + tokenTypeString(expected) + "', got " + (*_pos).toString() + ".\n";
+    panic(s);
   }
 }
--- src/gdl.hh
+++ src/gdl.hh
@@ -5,17 +5,64 @@
 #include "il.hh"
 
 namespace GDL {
+  enum TokenType {
+    IdentToken,
+    OpToken,
+    IntLitToken,
+    EOFToken
+  };
+
+  std::string tokenTypeString(TokenType);
+  
+  class Token {
+  public:
+    Token(TokenType, std::string);
+    Token(TokenType, int);
+
+    TokenType type() const;
+    std::string value() const;
+    int intValue() const;
+    std::string toString() const;
+
+  private:
+    TokenType _type;
+    std::string _value;
+    int _intValue;
+  };
+
+  class Tokeniser {
+  public:
+    Tokeniser(std::string&);
+
+    std::vector<Token> tokenise();
+    
+  private:
+    std::string::const_iterator _pos;
+    std::string::const_iterator _end;
+    std::vector<Token> _tokens;
+
+    void add(TokenType, std::string);
+    void add(TokenType, int);
+    void number();
+    void ident();
+    bool isDigit(char);
+    bool isLetter(char);
+    bool isWhiteSpace(char);
+  };
+  
   class Parser {
   public:
     Parser(std::string&);
 
+    void parseEOF();
     IL::Expression* parseExpression();
     IL::Expression* parseAddExpression();
     IL::IntConstExpression* parseIntConstExpression();
 
   private:
-    std::string::const_iterator _pos;
-    std::string::const_iterator _end;
+    std::vector<Token>::const_iterator _pos;
+
+    void error(TokenType) __attribute__((noreturn));
   };
 }
 
